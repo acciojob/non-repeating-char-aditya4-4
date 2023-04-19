8fc0c1a27f543348e434a7f9e4fafa45c9173f28
DURGESH KUMAR <68071906+aditya4-4@users.noreply.github.com>
2023-04-19 22:40:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let str = prompt("Enter a String");
+ function fisrtNonRepeatingChar(str){
+	 for(let i =0;i<str.length;i++){
+		 let count =0;
+		 for(let j=0;j<str.length;j++){
+			 if(str[i] == str[j]){
+				 count++;
+			 }
+		 }
+		 if(count == 1){
+			 return str[i];
+		 }
+	 }
+ }
+
+alert(fisrtNonRepeatingChar(str));
